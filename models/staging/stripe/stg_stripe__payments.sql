366768174cc866d9aa19dc159edf8733b2680657
alona808 <slonkina@datapoint.tips>
2026-02-21 21:03:04 +0200

    Created fct_orders table, refactored dim_customers table

---
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -0,0 +1,10 @@
+select
+        id as customer_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status,
+        -- amount is stored in cents, convert it to dollars
+        amount / 100 as amount,
+        created as created_at
+
+    from raw.stripe.payments
